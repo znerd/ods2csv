f995d8a01c49fe28f63cb2742b4c76aac6c3fcb1
Ernst de Haan <ernst@pensioenpage.com>
2009-09-01 17:03:39 +0200

    Unit tests actually work.

--- src/com/pensioenpage/jynx/ods2csv/tests/ConverterTests.java
+++ src/com/pensioenpage/jynx/ods2csv/tests/ConverterTests.java
@@ -7,6 +7,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -48,22 +50,49 @@ public class ConverterTests {
 
       // Test with actual input files, compare with expected output
       boolean finished = false;
-      int i = 0;
+      int i = 1;
       do {
-         InputStream ods = getClass().getResourceAsStream("test" + i + ".ods");
-         InputStream csv = getClass().getResourceAsStream("test" + i + ".csv");
+         byte[] ods = loadTestData(i, "ods");
+         byte[] csv = loadTestData(i, "csv");
 
          if (ods == null || csv == null) {
             finished = true;
          } else {
-            doTest(ods, csv);
+            doTest(i, ods, csv);
          }
+
+         i++;
       } while (! finished);
+
+      System.err.println("Ran " + (i - 1) + " test case(s).");
    }
 
-   private void doTest(InputStream ods, InputStream csv) throws Exception {
-      Converter converter = new Converter();
-      ByteArrayOutputStream actual = new ByteArrayOutputStream();
-      converter.convert(ods, actual);
+   private byte[] loadTestData(int index, String suffix) throws Exception {
+      InputStream byteStream = getClass().getResourceAsStream("test" + index + '.' + suffix);
+      return (byteStream == null) ? null : IOUtils.toByteArray(byteStream);
+   }
+
+   private void doTest(int testIndex, byte[] ods, byte[] expectedCSV) throws Exception {
+
+      // Prepare
+      Converter        converter = new Converter();
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+      // Convert
+      converter.convert(new ByteArrayInputStream(ods), baos);
+
+      // Convert to a byte array
+      byte[] actualCSV = baos.toByteArray();
+
+      // Loop over all bytes
+      int count = Math.min(expectedCSV.length, actualCSV.length);
+      String errorPrefix = "Test " + testIndex + ": Generated CSV is different from what was expected: ";
+      for (int i = 0; i < count; i++) {
+         byte expectedByte = expectedCSV[i];
+         byte   actualByte =   actualCSV[i];
+         assertTrue(errorPrefix + "At byte " + i + ": expected '" + expectedByte + "' (" + ((int) expectedByte) + ") instead of actual '" + actualByte + "' (" + ((int) actualByte) + ").", actualByte == expectedByte);
+      }
+
+      assertTrue(errorPrefix + "Expected " + expectedCSV.length + " byte(s) in output, but actual output contains " + actualCSV.length + " byte(s).", expectedCSV.length == actualCSV.length);
    }
 }
